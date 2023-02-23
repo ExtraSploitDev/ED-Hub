66f215ea016cb640e24ef8a9560b17e1ad133d6e
ExtraSploitDev <122784848+ExtraSploitDev@users.noreply.github.com>
2023-02-23 11:42:02 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 4954096313 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ExtraSploitDev/Field-TripZ-Script/main/FieldTripZMain.lua"))()
+
+elseif game.PlaceId == 4913581664 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ExtraSploitDev/E/main/Cart%20Ride%20Into%20Rdite%20Script.lua"))()
+   end
+end
